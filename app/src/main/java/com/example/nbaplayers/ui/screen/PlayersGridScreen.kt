6051f06b4dd47d53d114eb07bd2d2ee3e6f52847
Marek Alexa <marek.alexa1@gmail.com>
2025-06-17 00:57:57 +0200

    implement pull to refresh

--- app/src/main/java/com/example/nbaplayers/ui/screen/PlayersGridScreen.kt
+++ app/src/main/java/com/example/nbaplayers/ui/screen/PlayersGridScreen.kt
@@ -4,27 +4,26 @@ import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.nbaplayers.ui.component.PlayersGridList
 import com.example.nbaplayers.ui.viewmodel.PlayersViewModel
 
-
-@OptIn(ExperimentalFoundationApi::class, ExperimentalSharedTransitionApi::class)
+@OptIn(
+    ExperimentalFoundationApi::class,
+    ExperimentalSharedTransitionApi::class,
+    ExperimentalMaterial3Api::class
+)
 @Composable
 fun PlayersGridScreen(
     onPlayerClick: (Int) -> Unit,
@@ -32,38 +31,28 @@ fun PlayersGridScreen(
     animatedVisibilityScope: AnimatedVisibilityScope,
     viewModel: PlayersViewModel = hiltViewModel(),
 ) = with(sharedTransitionScope) {
-    val screenState = viewModel.screenState.collectAsState()
-    val players = screenState.value.players.collectAsLazyPagingItems()
-    rememberLazyGridState()
+    val players = viewModel.screenState.collectAsState().value.players.collectAsLazyPagingItems()
+    val refreshing = players.loadState.refresh is LoadState.Loading
+    val pullState = rememberPullToRefreshState()
 
-    val isRefreshing = players.loadState.refresh is LoadState.Loading
     players.loadState.append
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
 
-            PlayersGridList(
-                modifier = Modifier.weight(1f),
-                players = players,
-                onPlayerClick = onPlayerClick,
-                sharedTransitionScope = this@with,
-                animatedVisibilityScope = animatedVisibilityScope,
-            )
+            PullToRefreshBox(
+                isRefreshing = refreshing,
+                state = pullState,
+                onRefresh = { players.refresh() },
+                modifier = Modifier.fillMaxSize()
+            ) {
+                PlayersGridList(
+                    modifier = Modifier.fillMaxSize(),
+                    players = players,
+                    onPlayerClick = onPlayerClick,
+                    sharedTransitionScope = this@with,
+                    animatedVisibilityScope = animatedVisibilityScope,
+                )
+            }
         }
-
-        if (isRefreshing) {
-            LoaderIndicator()
-        }
-    }
-}
-
-@Composable
-private fun LoaderIndicator() {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.8f)),
-        contentAlignment = Alignment.Center,
-    ) {
-        CircularProgressIndicator(Modifier.padding(16.dp))
     }
 }
